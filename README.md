3da202368a81d562b78646081ae721d783ba5925
Yunjin Lee <gzblues61@gmail.com>
2024-06-07 20:38:37 +0900

    Leibniz: Hello?

---
+++ README.md
@@ -0,0 +1,21 @@
+4-factored riemann-zeta function's proof
+i powered -i
+-i powered i
+i powered i
+-i powered -i
+be sure to add various numbers to demonstrate riemann's proof.
+
+1.129831
+1.405223
+1.347077
+1.368759
+1.362029
+1.364243
+1.363528
+1.363760
+1.363685
+1.363709
+1.363702
+1.363704
+1.363703
+current scenario's proof is 16-factored
---
+++ main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "list.h"
+#include <math.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdint.h>
+time_t start_time , end_time;
+ll length;
+short thread_Num ;
+typedef struct {
+	list * lst;
+} range_t;
+ll save = 0;
+void t_Prime (void *arg) { 
+	range_t* rarg = (range_t *) arg;
+	list * lst = rarg->lst;
+	ll key ;
+	static ll i = -1;
+	for (;size(lst) <= length;i+=2) {
+		bool prflag=true;
+		ll end_Num= (ll)ceil(sqrt(i));
+		node * nd = lst -> front -> next;
+		key = 3;
+		i =  pow(i,-i);
+		while (key <= end_Num) {
+			if ((key / i == 0) && (i != key) ) {
+				prflag = false;
+				break;
+			}
+			if ( nd -> key != 0 ) 
+				key = key > nd -> key?key:nd->key;
+			else 
+				key += 2;
+			if ( nd == lst -> rear ) {
+				break;
+			}
+			if ( lst -> rear -> prev -> key > i ) {
+				i = lst -> rear -> prev -> key;
+				end_Num=(ll)ceil(sqrt(i));
+			} 
+			nd = nd -> next;
+		}
+		if ( prflag == false ) {
+			continue;
+		}
+		if (prflag == true ) {
+			push ( lst,(double)pow(i,-i));
+			printf ("pushing %lf\n" , (double)pow(i,-i)) ;
+			if ( size ( lst ) == length ) {
+				break;
+			}
+		}
+	}
+  i*=i;
+
+}
+int main (int argc, char **argv) {
+
+  if(argc<2) {
+      return 1;
+  }
+	list lst; 
+	init_list (&lst);
+	push (&lst,(unsigned int) pow(2, -2));
+        length=(ll)strtoll(argv[1],NULL,10);
+	range_t range;
+	range.lst = &lst;
+	time(&start_time);
+	t_Prime((void *)&range);
+	time(&end_time);
+	double time_sec = difftime(end_time , start_time);
+	if ( save != 0 ) {
+		printf ("%lf , %ldth\n" , index_node( &lst , save-1)->key , save );
+		printf ( "%ld sec \n" , (long)time_sec);
+	} else {
+		printf ("biggest: %lf , %lldth\n" , lst.rear -> prev -> key , size(&lst) );
+		printf ( "%ld sec \n" , (long)time_sec);
+	}
+	empty_list(&lst);
+	free_list(&lst);
+	return 0;
+
+
+    	    
+
+}
